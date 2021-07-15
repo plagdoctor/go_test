e5be29eddd831fc1d857b53ae49ce2c933e70239
PlagDoctor <danroom85@gmail.com>
2021-07-15 22:26:19 +0900

    4nd

--- main.go
+++ main.go
@@ -16,16 +16,44 @@ func lenAndUpper(a string) (wordLength int, wordUpper string) {
 func repeat(words ...string) {
 	fmt.Println(words)
 }
+
+func superAdd(numbers ...int) int {
+	total := 0
+	for i := 1; i <= 6; i++ {
+		println(i)
+	}
+
+	for _, number := range numbers {
+		total += number
+	}
+	println(total)
+	return 1
+}
+func canIDrink(age int) bool {
+	switch koreanAge := age; koreanAge {
+	case 18:
+		return true
+	case 10:
+		return true
+	}
+	return false
+
+}
 func main() {
-	fmt.Println("dd")
+	/*
+		fmt.Println("dd")
 
-	var name string = "jinyoung"
-	name = "nico"
+		var name string = "jinyoung"
+		name = "nico"
 
-	fmt.Println(name)
+		fmt.Println(name)
 
-	name_t_lengh, name_t_upper := lenAndUpper(name)
-	fmt.Println(name_t_lengh, name_t_upper)
-	repeat("mike", "jinyoung", "haha", "ilence")
+		name_t_lengh, name_t_upper := lenAndUpper(name)
+		fmt.Println(name_t_lengh, name_t_upper)
+		repeat("mike", "jinyoung", "haha", "ilence")
+	*/
+	//superAdd(1, 2, 3, 4, 5, 6)
 
+	println(canIDrink(18))
+	println(canIDrink(21))
 }
