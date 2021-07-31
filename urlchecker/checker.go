e926bb3c0dd330b609cd17c0b9cffb712c8c1c51
PlagDoctor <danroom85@gmail.com>
2021-07-31 09:54:53 +0900

    20210731_startScrapper

--- urlchecker/checker.go
+++ urlchecker/checker.go
@@ -6,45 +6,54 @@ import (
 	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 var errRequestFailed = errors.New("Request Failed")
 
 func main() {
+
+	c := make(chan requestResult)
+
 	urls := []string{
 		"https://www.reddit.com",
+		"https://www.google.com",
+		"https://nomadcoders.co",
 	}
 
-	var results = make(map[string]string)
-	// or var results = map[string]string{}
-
-	results["hello"] = "hello"
+	var results = map[string]string{}
 
 	for _, url := range urls {
-		result := "ok"
-		err := hitURL(url)
-		if err != nil {
-			result = "failed"
+
+		go hitURL(url, c)
+
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+	/*
+		results["hello"] = "hello"
+		for url, result := range results {
+			fmt.Println(url, result)
 		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	*/
 }
 
-func hitURL(url string) error {
+func hitURL(url string, c chan<- requestResult) {
 	fmt.Println("checking:", url)
+
 	resp, err := http.Get(url)
 
-	/*
-		data, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%s\n", string(data))
-	*/
-
 	if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
+		c <- requestResult{url: url, status: "Failed"}
+	} else {
+		c <- requestResult{url: url, status: "Ok"}
 	}
-	return nil
 }
